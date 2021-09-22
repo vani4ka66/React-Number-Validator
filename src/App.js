95558dee6941262f2d6efab0a422b501de4d15f0
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-22 14:11:39 +0300

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import "./App.css";
+import React, {useState, useMemo} from "react";
+// import {myForm, TextField} from '@react-component/my-form';
 
 function App() {
+
+  const [text, setText] = useState()
+  const [state, setState] = useState(false);
+
+  function handleChange(e){
+   setText(e.target.value)
+  }
+
+  const memoizedResult = useMemo(() => {
+    let compare = text;
+
+    const validInput = new RegExp('^[0-9]+$');
+
+    if(validInput.test(compare)){
+      return true
+    }
+    else{
+      return false
+    }
+  });
+
   return (
     <div className="App">
      <div className="control has-icons-right">
@@ -8,10 +31,11 @@ function App() {
           className="input is-large"
           type="text"
           placeholder="Enter number..."
+          onChange={handleChange}
 
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          {memoizedResult ?   <i className="fas fa-check" /> :   <i className="fas fa-times" /> }
         </span> 
       </div>
     </div>
